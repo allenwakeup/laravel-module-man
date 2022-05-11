e8320b594ec7ccd670f6a8f19fcb84db17b7d5cb
Dean <1602264241@qq.com>
2022-05-11 17:11:59 +0800

    修改app返回结构

--- src/Http/Controllers/Admin/AuthorizationsController.php
+++ src/Http/Controllers/Admin/AuthorizationsController.php
@@ -4,6 +4,7 @@ namespace Goodcatch\Modules\Manufacturing\Http\Controllers\Admin;
 
 use Goodcatch\Modules\Manufacturing\Http\Requests\Admin\AuthorizationRequest;
 use Illuminate\Auth\AuthenticationException;
+use App\Http\Controllers\Controller;
 
 class AuthorizationsController extends Controller
 {
@@ -15,29 +16,29 @@ class AuthorizationsController extends Controller
         $credentials['password'] = $request->password;
 
         if (!$token = \Auth::guard('app')->attempt($credentials)) {
-            return $this->errorResponse(401, trans('auth.failed'), 1001);
+            return $this->error(trans('auth.failed'),null);
         }
-        return $this->respondWithToken($token)->setStatusCode(201);
+        return $this->success($this->respondWithToken($token));
     }
 
     public function update()
     {
         $token = auth('app')->refresh();
-        return $this->respondWithToken($token);
+        return $this->success($this->respondWithToken($token));
     }
 
     public function destroy()
     {
         auth('app')->logout();
-        return response(null, 204);
+        return $this->success(null);
     }
 
     protected function respondWithToken($token)
     {
-        return response()->json([
+        return [
             'access_token' => $token,
             'token_type' => 'Bearer',
             'expires_in' => auth('app')->factory()->getTTL() * 60
-        ]);
+        ];
     }
 }
